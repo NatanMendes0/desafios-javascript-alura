2cd76a74c0fa1809747db7de789a6d77ce98c2cc
Natan Mendes <natancmendes@gmail.com>
2024-07-16 22:40:22 -0300

    fix identation

--- Compra de ingressos/js/app.js
+++ Compra de ingressos/js/app.js
@@ -7,16 +7,16 @@ function comprar() {
 
 // Decrementar numero das pistas
 function descontaIngresso(itemSelecionado, quantidade) {
-    
+
     //obtem o valor das opções
     const valorPista = document.getElementById('qtd-pista').textContent;
     const valorCadeiraSuperior = document.getElementById('qtd-superior').textContent;
     const valorCadeiraInferior = document.getElementById('qtd-inferior').textContent;
-    
+
     //verifica qual opção foi selecionada
     switch (itemSelecionado) {
         case 'pista':
-            
+
             //verifica se o valor da pista é 0
             if (valorPista == 0) {
                 alert('Ingressos esgotados');
@@ -24,7 +24,7 @@ function descontaIngresso(itemSelecionado, quantidade) {
             }
             //verifica se a quantidade é maior que o valor da pista
             else if (quantidade > valorPista) {
-                
+
                 mensagemCompraMaxima(quantidade, valorPista);
                 document.getElementById('qtd-pista').textContent = 0;
             }
@@ -33,33 +33,33 @@ function descontaIngresso(itemSelecionado, quantidade) {
                 document.getElementById('qtd-pista').textContent = valorPista - quantidade;
             }
             break;
-            
-            case 'superior':
-                //verifica se o valor da cadeira superior é 0
-                if (valorCadeiraSuperior == 0) {
-                    alert('Ingressos esgotados');
-                    document.getElementById('qtd-superior').value = 0;
-                }
-                //verifica se a quantidade é maior que o valor da cadeira superior
-                else if (quantidade > valorCadeiraSuperior) {
-                    mensagemCompraMaxima(quantidade, valorCadeiraSuperior);
-                    document.getElementById('qtd-superior').textContent = 0;
-                }
-                //caso contrário, decrementa o valor da cadeira superior
-                else {
-                    document.getElementById('qtd-superior').textContent = valorCadeiraSuperior - quantidade;
-                }
-                break;
-                
-                case 'inferior':
-                    //verifica se o valor da cadeira inferior é 0
-                    if (valorCadeiraInferior == 0) {
-                        alert('Ingressos esgotados');
-                        document.getElementById('qtd-inferior').value = 0;
-                    }
-                    //verifica se a quantidade é maior que o valor da cadeira inferior
-                    else if (quantidade > valorCadeiraInferior) {
-                        mensagemCompraMaxima(quantidade, valorCadeiraInferior);
+
+        case 'superior':
+            //verifica se o valor da cadeira superior é 0
+            if (valorCadeiraSuperior == 0) {
+                alert('Ingressos esgotados');
+                document.getElementById('qtd-superior').value = 0;
+            }
+            //verifica se a quantidade é maior que o valor da cadeira superior
+            else if (quantidade > valorCadeiraSuperior) {
+                mensagemCompraMaxima(quantidade, valorCadeiraSuperior);
+                document.getElementById('qtd-superior').textContent = 0;
+            }
+            //caso contrário, decrementa o valor da cadeira superior
+            else {
+                document.getElementById('qtd-superior').textContent = valorCadeiraSuperior - quantidade;
+            }
+            break;
+
+        case 'inferior':
+            //verifica se o valor da cadeira inferior é 0
+            if (valorCadeiraInferior == 0) {
+                alert('Ingressos esgotados');
+                document.getElementById('qtd-inferior').value = 0;
+            }
+            //verifica se a quantidade é maior que o valor da cadeira inferior
+            else if (quantidade > valorCadeiraInferior) {
+                mensagemCompraMaxima(quantidade, valorCadeiraInferior);
                 document.getElementById('qtd-inferior').textContent = 0;
             }
             //caso contrário, decrementa o valor da cadeira inferior
@@ -67,13 +67,13 @@ function descontaIngresso(itemSelecionado, quantidade) {
                 document.getElementById('qtd-inferior').textContent = valorCadeiraInferior - quantidade;
             }
             break;
-        }
-        
     }
-    
-    // Mensagem de compra máxima
-    function mensagemCompraMaxima(quantidade, valor) {
-        let quantidadeRestante = quantidade - valor;
-        let checaPlural = quantidadeRestante > 1 ? 'ingressos' : 'ingresso';
-        alert(`Quantidade máxima de ingressos atingida. Não foi possível comprar mais ${quantidadeRestante} ${checaPlural}.`);
-    }
+
+}
+
+// Mensagem de compra máxima
+function mensagemCompraMaxima(quantidade, valor) {
+    let quantidadeRestante = quantidade - valor;
+    let checaPlural = quantidadeRestante > 1 ? 'ingressos' : 'ingresso';
+    alert(`Quantidade máxima de ingressos atingida. Não foi possível comprar mais ${quantidadeRestante} ${checaPlural}.`);
+}
